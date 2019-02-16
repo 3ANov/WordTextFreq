fcffda360d019d19053875cbeee11c6ad2156428
3ANov <jeims0007@gmail.com>
2019-02-16 16:34:30 +0300

    Готово. Напишите программу, читающую из System.in текст в кодировке UTF-8, подсчитывающую в нем частоту появления слов, и в конце выводящую 10 наиболее часто встречающихся слов.
            Словом будем считать любую непрерывную последовательность символов, состоящую только из букв и цифр. Например, в строке "Мама мыла раму 33 раза!" ровно пять слов: "Мама", "мыла", "раму", "33" и "раза".
    
            Подсчет слов должен выполняться без учета регистра, т.е. "МАМА", "мама" и "Мама" — это одно и то же слово. Выводите слова в нижнем регистре.
    
            Если в тексте меньше 10 уникальных слов, то выводите сколько есть.
    
            Если в тексте некоторые слова имеют одинаковую частоту, т.е. их нельзя однозначно упорядочить только по частоте, то дополнительно упорядочите слова с одинаковой частотой в лексикографическом порядке.

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) throws IOException{
         //отладка
         //String input = "Мама мыла-мыла-мыла раму!";
-        String input ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        //String input ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.";
+        //ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
 
 
         //отладка
         //Reader inRead = new InputStreamReader(System.in,"UTF-8");
-        Reader inRead = new InputStreamReader(inputStream,"UTF-8");
+        Reader inRead = new InputStreamReader(System.in,"UTF-8");
 
         BufferedReader bf = new BufferedReader(inRead);
 
@@ -29,9 +29,9 @@ public class Main {
 
         //HashMap<String, Long> map = new HashMap<String, Long>();
         words.entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                
-                .forEach(System.out::println);
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.<String, Long>comparingByKey()))
+                .limit(10)
+                .forEach(t->System.out.println(t.getKey()));
         /*
         for(String word:words.keySet()){
             System.out.println(word + " - " + words.get(word));
